d56f52f9f438ffbbedff8e9fe780adf7964cd313
shanyou <shanyou@htyunwang.com>
2020-05-09 18:37:09 +0800

    add cli main

--- prometheus_web_monitor/cli/README.md
+++ prometheus_web_monitor/cli/README.md
@@ -1,3 +1,13 @@
 web monitor cli
 ===
 使用cli控制添加删除监控站点
+
+
+## 参考
+[python argparse用法总结](https://www.jianshu.com/p/fef2d215b91d)
+
+[Python argparse 子命令](https://www.jianshu.com/p/27ce67dab97e)
+
+[argparse --- 命令行选项、参数和子命令解析器](https://docs.python.org/zh-cn/3/library/argparse.html)
+
+[Python @staticmethod@classmethod用法](https://blog.csdn.net/sinat_34079973/article/details/53502348)
--- prometheus_web_monitor/cli/main.py
+++ prometheus_web_monitor/cli/main.py
@@ -1,7 +1,31 @@
-#!/usr/bin/env python
-# encoding: utf-8
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 
 import argparse
-parser = argparse.ArgumentParser()
-parser.parse_args()
+def add(args):
+    """ add monitor site """
+    print('call add')
+    print(args)
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-c', '--config', help='config file path for cli', required=False)
+    subparsers = parser.add_subparsers(help='manage monitor target for site')
+    parser_add = subparsers.add_parser('add', help='add monitor site')
+    parser_remove = subparsers.add_parser('remove', help='remove monitor site by name')
+    parser_list = subparsers.add_parser('list', help='list monitor site')
+
+    parser_add.add_argument('-n', '--name', help='name of site', dest='name')
+    parser_add.set_defaults(func=add)
+
+    args = parser.parse_args()
+    try:
+        args.func(args)
+    except AttributeError:
+        parser.print_help()
+        parser.exit()
+
+if __name__ == "__main__":
+    main()
