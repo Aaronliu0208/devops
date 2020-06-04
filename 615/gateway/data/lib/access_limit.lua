b354e46ad284b49cb69c8280fd191635c9587ccc
shanyou <shanyou@htyunwang.com>
2020-06-04 18:34:26 +0800

    add limit lua script

---
+++ 615/gateway/data/lib/access_limit.lua
@@ -0,0 +1,29 @@
+local limit_count = require "resty.limit.count"
+local rate_pre_second = 100
+-- rate: 100 requests per 1s
+local lim, err = limit_count.new("limit_conn_store", rate_pre_second, 1)
+if not lim then
+    ngx.log(ngx.ERR, "failed to instantiate a resty.limit.count object: ", err)
+    return ngx.exit(500)
+end
+-- use the Authorization header as the limiting key
+local key = ngx.req.get_headers()["Authorization"] or "public"
+local delay, err = lim:incoming(key, true)
+if not delay then
+    if err == "rejected" then
+        ngx.header["X-RateLimit-Limit"] = tostring(rate_pre_second)
+        ngx.header["X-RateLimit-Remaining"] = 0
+            --每秒请求超过100次的就报错403
+        return ngx.exit(403)
+    end
+    ngx.log(ngx.ERR, "failed to limit count: ", err)
+
+    return ngx.exit(500)
+end
+
+    -- the 2nd return value holds the current remaining number
+-- of requests for the specified key.
+local remaining = err
+
+ngx.header["X-RateLimit-Limit"] = tostring(rate_pre_second)
+ngx.header["X-RateLimit-Remaining"] = remaining
