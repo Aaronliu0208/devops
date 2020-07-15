820ae1a07b11c0d116cc332e0c1865c714e5191e
shanyou <shanyou@htyunwang.com>
2020-07-15 16:30:53 +0800

    add multi stdout test

--- marco/resources/resty_test.go
+++ marco/resources/resty_test.go
@@ -1,7 +1,9 @@
 package resources
 
 import (
+	"io"
 	"os"
+	"os/exec"
 	"testing"
 )
 
@@ -19,4 +21,21 @@ func TestRestyInstaller(t *testing.T) {
 		t.Fatal(err)
 	}
 	os.RemoveAll(path + "/test")
+}
+
+func TestMultiStdout(t *testing.T) {
+	// Logging capability
+	f, err := os.OpenFile("log.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		t.Fatalf("Error opening file: %v", err)
+	}
+	defer f.Close()
+	mwriter := io.MultiWriter(f, os.Stdout)
+	cmd := exec.Command("ls")
+	cmd.Stderr = mwriter
+	cmd.Stdout = mwriter
+	err = cmd.Run() //blocks until sub process is complete
+	if err != nil {
+		t.Fatal(err)
+	}
 }
