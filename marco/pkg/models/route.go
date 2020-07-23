fa38a1e9f65d45ace329cfefbcf553fd0d2f3c5c
shanyou <shanyou@htyunwang.com>
2020-07-23 17:44:43 +0800

    add site struct define

--- marco/pkg/models/route.go
+++ marco/pkg/models/route.go
@@ -2,4 +2,19 @@ package models
 
 //Route represent nginx location
 type Route struct {
+	/* 匹配符号
+	https://blog.csdn.net/yuan_xw/article/details/51333451
+	= 开头表示精确匹配
+	"" 空字符串 默认匹配
+	^~开头表示uri以某个常规字符串开头，理解为匹配 url路径即可。
+	nginx不对url做编码，因此请求为/static/20%/aa，
+	可以被规则^~ /static/ /aa匹配到（注意是空格）
+	~ 开头表示区分大小写的正则匹配
+	~* 开头表示不区分大小写的正则匹配
+	!~和!~*分别为区分大小写不匹配及不区分大小写不匹配的正则
+	*/
+	Pattern string `kv:"-"`
+	// Path 正则表达式匹配的路径
+	Path string `kv:"-"`
+	Root string `kv:"root,omitempty"`
 }
--- marco/pkg/models/site.go
+++ marco/pkg/models/site.go
@@ -1,14 +1,28 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"casicloud.com/ylops/marco/pkg/nginx"
+)
 
 //Site represent nginx server config
 type Site struct {
 	ID        string
-	CreatedAt time.Time   `json:"created_at"`
-	UpdatedAt time.Time   `json:"updated_at"`
-	Domain    string      `kv:"server_name"`
-	EnableSSL bool        `kv:"-"`
-	Cert      Certificate `kv:"-"`
+	CreatedAt time.Time `json:"created_at"  kv:"-"`
+	UpdatedAt time.Time `json:"updated_at" kv:"-"`
+	Port      int
+	Domain    string
+	EnableSSL bool
+	Cert      Certificate
 	Routes    []Route
+	Root      string
+	AccessLog string
+	ErrorLog  string
+	Extras    nginx.Options
+}
+
+//Marshal implements directive Marshaler
+func (s Site) Marshal() ([]nginx.Directive, error) {
+	return nil, nil
 }
