8d6b95796d0043f7dc4abc58835d6a652b6a31eb
shanyou <shanyou@htyunwang.com>
2020-07-28 10:22:26 +0800

    add find block test

--- marco/pkg/nginx/blocks_test.go
+++ marco/pkg/nginx/blocks_test.go
@@ -15,3 +15,45 @@ func TestEmptyBlock(t *testing.T) {
 	assert.True(t, strings.Contains(blkStr, "proxy_set_header"))
 	assert.True(t, strings.Contains(blkStr, "X-Real-IP $remote_addr"))
 }
+
+func TestFindBlock(t *testing.T) {
+	emptyBlk := NewEmptyBlock()
+
+	blk := NewBlock("test")
+	blk.AddKVOption("foo", "bar")
+	sblk := NewBlock("sub")
+	sblk.AddKVOption("aa", "bb")
+	blk.AddDirective(sblk)
+	emptyBlk.AddDirective(blk)
+
+	fb, err := emptyBlk.FindDirectiveByName("aa")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.True(t, fb.Name() == "aa")
+
+	fb, err = emptyBlk.FindDirectiveByName("foo")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.True(t, fb.Name() == "foo")
+
+	fb, err = emptyBlk.FindDirectiveByName("sub")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.True(t, fb.Name() == "sub")
+}
+
+func TestFindBlockFail(t *testing.T) {
+	emptyBlk := NewEmptyBlock()
+	blk := NewBlock("test")
+	blk.AddKVOption("foo", "bar")
+	sblk := NewBlock("sub")
+	sblk.AddKVOption("aa", "bb")
+	blk.AddDirective(sblk)
+	emptyBlk.AddDirective(blk)
+	fb, err := emptyBlk.FindDirectiveByName("abc")
+	assert.NotNil(t, err)
+	assert.Nil(t, fb)
+}
