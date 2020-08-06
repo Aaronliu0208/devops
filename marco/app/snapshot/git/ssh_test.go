c0aa03a00c4c220ca8307550d91303e0f244c201
shanyou <shanyou@htyunwang.com>
2020-08-06 17:02:40 +0800

    add clone test

--- marco/app/snapshot/git/ssh_test.go
+++ marco/app/snapshot/git/ssh_test.go
@@ -3,6 +3,7 @@ package git
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -21,59 +22,98 @@ import (
 	"github.com/go-git/go-git/v5/storage/memory"
 )
 
-var remoteRepo string = "git@yunludev3.htyunlu.com:shanyou/marco-snapshot.git"
+var remoteURL string = "git@yunludev3.htyunlu.com:shanyou/marco-snapshot.git"
 
-func TestSSHKeyload(t *testing.T) {
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+func getAuth() (transport.AuthMethod, error) {
 	_, filename, _, _ := runtime.Caller(0)
 	wd := filepath.Dir(filename)
 	s := fmt.Sprintf("%s/test/id_rsa", wd)
-	localRepo := fmt.Sprintf("%s/test/localrepo", wd)
 	sshKey, err := ioutil.ReadFile(s)
 	if err != nil {
-		t.Fatal(err)
+		return nil, err
 	}
 
 	pubkey, err := ssh2.NewPublicKeys("git", []byte(sshKey), "1234.asd")
 	if err != nil {
-		t.Fatal(err)
+		return pubkey, err
 	}
 
-	rem := git.NewRemote(memory.NewStorage(), &config.RemoteConfig{
+	return pubkey, err
+}
+
+func getRepoDir() string {
+	_, filename, _, _ := runtime.Caller(0)
+	wd := filepath.Dir(filename)
+	return fmt.Sprintf("%s/test/localrepo", wd)
+}
+
+func CreateRemote() *git.Remote {
+	return git.NewRemote(memory.NewStorage(), &config.RemoteConfig{
 		Name: "origin",
-		URLs: []string{remoteRepo},
+		URLs: []string{remoteURL},
 	})
+}
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	localRepo := getRepoDir()
+	os.RemoveAll(localRepo)
+	os.MkdirAll(localRepo, 0777)
+}
+
+func destroy() {
+	localRepo := getRepoDir()
+	os.RemoveAll(localRepo)
+	os.MkdirAll(localRepo, 0777)
+}
+
+func checkIfError(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSSHKeyload(t *testing.T) {
+	defer destroy()
+	localRepo := getRepoDir()
+	pubkey, err := getAuth()
+	checkIfError(t, err)
+
+	rem := CreateRemote()
 	_, err = rem.List(&git.ListOptions{
 		Auth: pubkey,
 	})
 	if err != nil && err != transport.ErrEmptyRemoteRepository {
 		t.Fatal(err)
 	}
+
+	var isEmpty bool = false
 	if err == transport.ErrEmptyRemoteRepository {
 		fmt.Println("empty repo")
+		isEmpty = true
 	}
 	wt := osfs.New(localRepo)
 	dot, _ := wt.Chroot(".git")
 	fs := filesystem.NewStorage(dot, cache.NewObjectLRUDefault())
 	repo, err := git.Init(fs, wt)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		// os.RemoveAll(localRepo + "/.git")
-	}()
+	checkIfError(t, err)
+
 	_, err = repo.CreateRemote(&config.RemoteConfig{
 		Name: "origin",
-		URLs: []string{remoteRepo},
+		URLs: []string{remoteURL},
 	})
-
-	if err != nil {
-		t.Fatal(err)
-	}
+	checkIfError(t, err)
 
 	list, err := repo.Remotes()
-	if err != nil {
-		t.Fatal(err)
-	}
+	checkIfError(t, err)
 
 	for _, r := range list {
 		fmt.Println(r)
@@ -96,9 +136,7 @@ func TestSSHKeyload(t *testing.T) {
 			return nil
 		})
 	status, err := w.Status()
-	if err != nil {
-		t.Fatal(err)
-	}
+	checkIfError(t, err)
 	fmt.Println(status)
 
 	commit, err := w.Commit("example go-git commit", &git.CommitOptions{
@@ -109,22 +147,82 @@ func TestSSHKeyload(t *testing.T) {
 		},
 	})
 
-	if err != nil {
-		t.Fatal(err)
-	}
+	checkIfError(t, err)
 	// Prints the current HEAD to verify that all worked well.
 
 	obj, err := repo.CommitObject(commit)
 	fmt.Println(obj)
-	if err != nil {
-		t.Fatal(err)
+	checkIfError(t, err)
+
+	if !isEmpty {
+		// fetch and merge
+		err = repo.Fetch(&git.FetchOptions{
+			RemoteName: "origin",
+			Auth:       pubkey,
+		})
+
+		checkIfError(t, err)
+
+		err = w.Pull(&git.PullOptions{RemoteName: "origin",
+			Auth: pubkey})
+		checkIfError(t, err)
+
 	}
 
 	err = repo.Push(&git.PushOptions{
 		Auth: pubkey,
 	})
 
-	if err != nil {
+	checkIfError(t, err)
+}
+
+func TestCheckout(t *testing.T) {
+	defer destroy()
+	rem := CreateRemote()
+	auth, err := getAuth()
+	checkIfError(t, err)
+	refs, err := rem.List(&git.ListOptions{
+		Auth: auth,
+	})
+
+	fmt.Printf("%v\n", refs)
+	if err != nil && err != transport.ErrEmptyRemoteRepository {
 		t.Fatal(err)
 	}
+	localRepo := getRepoDir()
+	r, err := git.PlainClone(localRepo, false, &git.CloneOptions{
+		Auth:       auth,
+		NoCheckout: true,
+		Progress:   os.Stdout,
+		URL:        remoteURL,
+	})
+	checkIfError(t, err)
+	fmt.Printf("%v\n", r)
+	wt, err := r.Worktree()
+	checkIfError(t, err)
+	rnd := randString(10)
+	ioutil.WriteFile(filepath.Join(localRepo, "test.log"), []byte(rnd), 0644)
+	status, err := wt.Status()
+	checkIfError(t, err)
+	for n, _ := range status {
+		wt.Add(n)
+	}
+	commit, err := wt.Commit("commit with:"+rnd, &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  "shanyou",
+			Email: "shanyou@htyunwang.com",
+			When:  time.Now(),
+		},
+	})
+
+	checkIfError(t, err)
+	obj, err := r.CommitObject(commit)
+	fmt.Println(obj)
+	checkIfError(t, err)
+	err = r.Push(&git.PushOptions{
+		Auth: auth,
+	})
+
+	checkIfError(t, err)
+
 }
